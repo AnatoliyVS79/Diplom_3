c903087b282796f09f379ff296c25a34ac4eca4b
Anatoliy Samoylov <a9295730700@yandex.ru>
2023-04-28 12:01:51 +0300

    added UsersSteps

---
+++ src/main/java/ru/praktikum/stellarburgers/api/UsersSteps.java
@@ -0,0 +1,10 @@
+package ru.praktikum.stellarburgers.api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class UsersSteps {
+    public static final RequestSpecification REQUEST_SPECIFICATION =
+            new RequestSpecBuilder()
+                    .setBaseUri()
+}
