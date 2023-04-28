ed4f14659ed0b6df40309ed2fa9ba449f36ed3de
Anatoliy Samoylov <a9295730700@yandex.ru>
2023-04-28 12:05:12 +0300

    added ForgotPasswordPage

---
+++ src/main/java/ru/praktikum/stellarburgers/constants/ForgotPasswordPage.java
@@ -0,0 +1,33 @@
+package ru.praktikum.stellarburgers.constants;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ForgotPasswordPage {
+    WebDriver driver;
+    @FindBy(xpath = "//input[@name='name']")
+    WebElement emailField;
+    @FindBy(xpath = "//button[text()='Восстановить']")
+    WebElement recoverButton;
+    @FindBy(xpath = "//a[@href='/login']")
+    WebElement signInButton;
+
+    public ForgotPasswordPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterEmail(String email) {
+        emailField.sendKeys(email);
+    }
+
+    public void clickRecoverButton() {
+        recoverButton.click();
+    }
+
+    public void clickSignInButton() {
+        signInButton.click();
+    }
+}
