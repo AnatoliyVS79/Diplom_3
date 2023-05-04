a061801b1520c396e0243a99c17f53ad856f110f
Anatoliy Samoylov <a9295730700@yandex.ru>
2023-05-04 19:11:23 +0300

    changes in DriverInitializer class

--- src/main/java/ru/praktikum/stellarburgers/utils/DriverInitializer.java
+++ src/main/java/ru/praktikum/stellarburgers/utils/DriverInitializer.java
@@ -1,24 +1,36 @@
 package ru.praktikum.stellarburgers.utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.praktikum.stellarburgers.constants.Browser;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverInitializer {
-    public static WebDriver getDriver(Browser browser) {
-        switch (browser) {
-            case CHROME:
-                WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
+    private  static final String YANDEX_DRIVER_PATH = String.format("%s/%s",
+            System.getenv("WebDriver"), "chromedriver-yandex");
+    private  static final String YANDEX_BROWSER_PATH =
+            "C:\\Users\\Anatoliy\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe";
 
-            case YANDEX:
-                System.setProperty("webdriver.chrome.driver",
-                        "src/test/resources/yandexdriver.exe");
-                return new ChromeDriver();
-
-            default:
-                throw new EnumConstantNotPresentException(Browser.class, "BROWSER");
+    public static WebDriver createWebDriver(){
+        String browser = System.getProperty("browser");
+        if (browser == null){
+            return createChromeDriver();
         }
+        switch (browser){
+            case "yandex":
+                return createYandexDriver();
+            case "chrome":
+            default:
+                return createChromeDriver();
+        }
+    }
+    private static WebDriver createChromeDriver(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        return new ChromeDriver(chromeOptions);
+    }
+    private static WebDriver createYandexDriver(){
+        System.setProperty("webdriver.chrome.driver", YANDEX_DRIVER_PATH);
+        ChromeOptions yandexOptions =new ChromeOptions();
+        yandexOptions.setBinary(YANDEX_BROWSER_PATH);
+        return new ChromeDriver(yandexOptions);
     }
 }
